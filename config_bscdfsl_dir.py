eb6138d944176746a030f588242d82c5a94944c9
lqjoy <132069684+lqjoy@users.noreply.github.com>
2025-11-19 21:04:35 +0800

    Add files via upload

---
+++ config_bscdfsl_dir.py
@@ -0,0 +1,14 @@
+save_dir                    = './logs'
+
+# miniImageNet_path           	= '/ssd/miniImagenet'
+miniImageNet_path           	= r'E:\mycode\CDFS\cdfsl-benchmark-master\ssd\miniImagenet'
+# DTD_path 						= '/ssd/dtd/images/'
+
+# ISIC_path = "/ssd/data/ISIC"
+# ChestX_path = "/ssd/data/chestX"
+# CropDisease_path = "/ssd/data/CropDiseases"
+# EuroSAT_path = "/ssd/data/EuroSAT/2750"
+ISIC_path = r'E:\mycode\CDFS\cdfsl-benchmark-master\ssd\data\ISIC'
+ChestX_path = r'E:\mycode\CDFS\cdfsl-benchmark-master\ssd\data\chestX'
+CropDisease_path = r'E:\mycode\CDFS\cdfsl-benchmark-master\ssd\data\CropDiseases'
+EuroSAT_path = r'E:\mycode\CDFS\cdfsl-benchmark-master\ssd\data\EuroSAT\2750'
