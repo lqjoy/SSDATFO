e8135c3fdca347e5d0743a400c2553723761c81e
lqjoy <132069684+lqjoy@users.noreply.github.com>
2025-11-19 21:05:42 +0800

    Update dataset paths in config_bscdfsl_dir.py

--- config_bscdfsl_dir.py
+++ config_bscdfsl_dir.py
@@ -1,14 +1,10 @@
 save_dir                    = './logs'
 
-# miniImageNet_path           	= '/ssd/miniImagenet'
-miniImageNet_path           	= r'E:\mycode\CDFS\cdfsl-benchmark-master\ssd\miniImagenet'
-# DTD_path 						= '/ssd/dtd/images/'
+miniImageNet_path           	= '/ssd/miniImagenet'
 
-# ISIC_path = "/ssd/data/ISIC"
-# ChestX_path = "/ssd/data/chestX"
-# CropDisease_path = "/ssd/data/CropDiseases"
-# EuroSAT_path = "/ssd/data/EuroSAT/2750"
-ISIC_path = r'E:\mycode\CDFS\cdfsl-benchmark-master\ssd\data\ISIC'
-ChestX_path = r'E:\mycode\CDFS\cdfsl-benchmark-master\ssd\data\chestX'
-CropDisease_path = r'E:\mycode\CDFS\cdfsl-benchmark-master\ssd\data\CropDiseases'
-EuroSAT_path = r'E:\mycode\CDFS\cdfsl-benchmark-master\ssd\data\EuroSAT\2750'
+DTD_path 						= '/ssd/dtd/images/'
+
+ISIC_path = "/ssd/data/ISIC"
+ChestX_path = "/ssd/data/chestX"
+CropDisease_path = "/ssd/data/CropDiseases"
+EuroSAT_path = "/ssd/data/EuroSAT/2750"
